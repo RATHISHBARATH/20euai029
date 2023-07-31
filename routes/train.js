e7e4d733d63eaefa9dbc9147be226eaf8b5d256f
RATHISHBARATH <94107495+RATHISHBARATH@users.noreply.github.com>
2023-07-31 20:45:31 +0530

    Update train.js

--- routes/train.js
+++ routes/train.js
@@ -3,10 +3,10 @@ import Train from '../model/train.js';
 
 const router = express.Router();
 
-// Route for getting the train data
+
 router.get('/', async (req, res) => {
   try {
-    // Find the train with its coach and bookings data
+
     const train = await Train.findOne()
       .select('-__v')
       .populate('coach.seats', '-_id -__v')
@@ -16,10 +16,9 @@ router.get('/', async (req, res) => {
       return res.status(404).json({ message: 'Train not found' });
     }
 
-    // Map the seats data to a simpler array of boolean values indicating whether the seat is booked or not
+
     const seats = train.coach.seats.map((seat) => seat.isBooked);
 
-    // Send the train data along with the simplified seats data to the client
     res.json({ train, seats });
   } catch (err) {
     console.error(err);
@@ -27,10 +26,10 @@ router.get('/', async (req, res) => {
   }
 });
 
-// Route for allowing any user to book seats
+
 router.post('/', async (req, res) => {
   try {
-    // Parse the requested number of seats from the client
+  
     const numSeats = parseInt(req.body.numSeats);
 
     if (!numSeats || numSeats < 1 || numSeats > 7) {
@@ -39,7 +38,7 @@ router.post('/', async (req, res) => {
         .json({ message: 'Invalid number of seats requested' });
     }
 
-    // Find the train with its coach and bookings data
+   
     const train = await Train.findOne()
       .populate('coach.seats')
       .populate('bookings');
@@ -48,14 +47,13 @@ router.post('/', async (req, res) => {
       return res.status(404).json({ message: 'Train not found' });
     }
 
-    // Find the available seats for the requested number of seats
+   
     let availableSeats = findAvailableSeats(train.coach.seats, numSeats);
 
     if (!availableSeats) {
       return res.status(400).json({ message: 'No seats available' });
     }
 
-    // Mark the available seats as booked
     markSeatsAsBooked(train, availableSeats);
 
     // Create a new booking with the booked seat numbers
